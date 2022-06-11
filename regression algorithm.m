0e3f815e127b5fc071678f447c3103d1b57fcc04
sunho45 <100903674+sunho45@users.noreply.github.com>
2022-06-11 21:53:29 +0900

    Update regression algorithm.m

--- regression algorithm.m
+++ regression algorithm.m
@@ -1,28 +1,55 @@
-dx=[2 4 6 8 2  ]
+
+dx=[2 4 6 8 10 ]
 dxi=[2 4 6 8 2  ]
-dy=[81 93 91 97 95  ]
+dy=[15 30 45 60 75  ]
 dyi=[2 4 6 8 2  ]
 syms x
 a=0;
 b=0;
-rate=0.025;
+T=50;
+da=0;
+db=0;
 
-n=1001;
+
+n=10000;
 
 for num=1:n
 
-predict=a*dx+b;
-realgap=predict-dy;
-adiff=2*mean(realgap.*dx);
-bdiff=2*mean(realgap);
+u=-1+2*rand();
+t=-1+2*rand();
+ da=a+T*u;
+ db=a+T*t;
+predict=a*dx;
 
-a=a-rate*adiff;
-b=b-rate*bdiff;
+predictl=da*dx;
+realgap=predict-dy;
+mse=mean((realgap).^2);
+
+realgapl=predictl-dy;
+msel=mean((realgapl).^2);
+
+d=msel-mse;
+if(d<0)
+    a=da;
+    
+else
+q=rand;
+p=exp(-d/T);
+if(q<p)
+a=da;
+
+end
+end
+
+
+
+
+
 
 figure(1)
 
 plot(dx,dy,'r.','MarkerSize',25) 
-axis([0 14 50 110]),title(["학습률: 0.025,횟수=",num2str(num)     ]),xlabel("선호가 공부하는 시간"),ylabel("선호의 시험성적 "),hold on
+axis([0 14 0 100]),title(["학습률: 0.025,횟수=",num2str(num)     ]),xlabel("선호가 공부하는 시간"),ylabel("선호의 시험성적 "),hold on
 
 y=a*x+b;
 fplot(y,'b')
@@ -30,8 +57,11 @@ hold off
 drawnow
 figure(2)
 
-
+T=0.90*T;
 
 
 
 end
+
+
+
