2e6b64a49cfd20bfbed58ecc4513fba5c31cff41
sunho45 <100903674+sunho45@users.noreply.github.com>
2022-06-12 00:00:11 +0900

    Update regression algorithm.m

--- regression algorithm.m
+++ regression algorithm.m
@@ -1,8 +1,7 @@
+dx=[2 4 6 8 10 2 6 8 10 12 14 16 18 16 18 18]
 
-dx=[2 4 6 8 10 ]
-dxi=[2 4 6 8 2  ]
-dy=[10 20 30 40 50  ]
-dyi=[2 4 6 8 2  ]
+dy=[10 20 30 40 50 30 40 20 60 65 60 70 70 65 80 75 ]
+
 syms x
 a=0;
 b=0;
@@ -11,7 +10,7 @@ da=0;
 db=0;
 
 
-n=10000;
+n=1000;
 
 for num=1:n
 
@@ -73,10 +72,11 @@ end
 
 
 end
+
 figure(1)
 
 plot(dx,dy,'r.','MarkerSize',25) 
-axis([-14 14 -100 100]),title(["학습률: 0.025,횟수=",num2str(num)     ]),xlabel("선호가 공부하는 시간"),ylabel("선호의 시험성적 "),hold on
+axis([0 20 0 100]),title(["횟수=",num2str(num),"온도=",num2str(T),"y=",num2str(a),"X+",num2str(b)   ]),xlabel("선호가 공부하는 시간"),ylabel("선호의 시험성적 "),hold on
 
 y=a*x+b;
 fplot(y,'b')
@@ -84,6 +84,8 @@ hold off
 drawnow
 figure(2)
 T=0.99*T;
+
+
 end
 
 
